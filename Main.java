9824441b2e6cfee28fb227819c9f270430c5838e
MateuszWr <92813166+MateuszWr@users.noreply.github.com>
2023-04-19 14:38:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    for(int=0;i<h;i++){
+      for(intk=0;k<=i;k++){
+        system.out.print("*");
+      }
+      for(intj=1;j<5-i;j++){
+        system.out.print(" ");
+      }
+      System.out.println();
+
+
+      
+    }
+  }
+}
